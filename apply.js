f6fd35270377c9342b67e88f168b95453c41fb4a
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2025-02-08 22:43:58 +0530

    Update apply.js

--- apply.js
+++ apply.js
@@ -3,12 +3,16 @@ Function.prototype.myApply = function(context, args) {
   if(typeof this !== 'function') {
     throw new TypeError('myApply must be called on a function.');
   }
-
-  // Set the context to the global object if null or undefined
-  const context = (context !== null && context !== undefined) ? Object(context) : globalThis;
+  
+  if (args === null || args === undefined) {
+    args = [];
+  }
   // check wheather second argument is an array or not
   if(!Array.isArray(args)) throw new Error('second argument should be an array');
   // Use a unique key to avoid property collisions
+
+  // Set the context to the global object if null or undefined
+  const context = (context !== null && context !== undefined) ? Object(context) : globalThis;
   const uniqueKey = Symbol('__tempFund');
   context[uniqueKey] = this;
   
